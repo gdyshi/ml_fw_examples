3e5bca386b893655f491e64db42112e0d1ccab3d
gdyshi <gdyshi@126.com>
2017-10-18 17:00:39 +0800

    可正常运行

--- gdyshi/transfer_learning.py
+++ gdyshi/transfer_learning.py
@@ -19,22 +19,25 @@ BOTTLENECK_TENSOR_NAME = 'pool_3/_reshape:0'
 # 图像输入张量所对应的名称。
 JPEG_DATA_TENSOR_NAME = 'DecodeJpeg/contents:0'
 
+COMMON_DIR = 'F:\\tmp\\bdci\\fangyi'
+
 # 下载的谷歌训练好的Inception-v3模型文件目录
-MODEL_DIR = 'E:\data\model'
+MODEL_DIR = COMMON_DIR + '\model'
 
 # 下载的谷歌训练好的Inception-v3模型文件名
 MODEL_FILE = 'tensorflow_inception_graph.pb'
 
 # 因为一个训练数据会被使用多次，所以可以将原始图像通过Inception-v3模型计算得到的特征向量保存在文件中，免去重复的计算。
 # 下面的变量定义了这些文件的存放地址。
-CACHE_DIR = 'E:\data\\bottleneck'
-
+CACHE_DIR = COMMON_DIR + '\\train\\bottleneck'
+# 训练参数保存路径
+PARM_FILE = COMMON_DIR + '\\train\parm\\variable.ckpt'
 # 图片数据文件夹。
 # 在这个文件夹中每一个子文件夹代表一个需要区分的类别，每个子文件夹中存放了对应类别的图片。
-INPUT_DATA = 'E:\data\\training'
-INPUT_LABLE = 'E:\data\lable'
-# INPUT_DATA = 'F:\\tmp\\bdci\\fangyi\\train\\training'
-# INPUT_LABLE = 'F:\\tmp\\bdci\\fangyi\\train\lable'
+# INPUT_DATA = 'E:\data\\training'
+# INPUT_LABLE = 'E:\data\lable'
+INPUT_DATA = COMMON_DIR + '\\train\\training'
+INPUT_LABLE = COMMON_DIR + '\\train\lable'
 
 # 验证的数据百分比
 VALIDATION_PERCENTAGE = 10
@@ -98,6 +101,7 @@ def get_data_set():
     youlun_list = []
     youting_list = []
     yuchuan_list = []
+    print(file_list)
     for file in file_list:
         f = open(file)  # 返回一个文件对象
         line = f.readline()  # 调用文件的 readline()方法
@@ -114,11 +118,13 @@ def get_data_set():
                 yuchuan_list.append(get_real_img_name(listFromLine[0]))
             line = f.readline()
         f.close()
+    print(huochuan_list)
     data_set['huochuan'] = huochuan_list
     data_set['youlun'] = youlun_list
     data_set['youting'] = youting_list
     data_set['yuchuan'] = yuchuan_list
     return data_set
+
 
 # 替换标记文件中记录的图片路径为真实的图片路径
 def get_real_img_name(old_path):
@@ -157,18 +163,17 @@ def get_image(image_lists, label_name, index, category):
 def get_bottlenect(image_lists, label_name, index, category):
     # 获取图片的文件名。
     image_full_name = get_image(image_lists, label_name, index, category)
-    bottlenect_full_name = image_full_name.replace(INPUT_DATA,CACHE_DIR)
-    return bottlenect_full_name + '.txt';
+    bottlenect_full_name = image_full_name.replace(INPUT_DATA, CACHE_DIR)
+    return bottlenect_full_name + '.txt'
 
 
 # 这个函数使用加载的训练好的Inception-v3模型处理一张图片，得到这个图片的特征向量。
 def run_bottleneck_on_image(sess, image_data, image_data_tensor, bottleneck_tensor):
-    # # 这个过程实际上就是将当前图片作为输入计算瓶颈张量的值。这个瓶颈张量的值就是这张图片的新的特征向量。
-    # bottleneck_values = sess.run(bottleneck_tensor, {image_data_tensor: image_data})
-    # # 经过卷积神经网络处理的结果是一个四维数组，需要将这个结果压缩成一个特征向量（一维数组）
-    # bottleneck_values = np.squeeze(bottleneck_values)
-    # return bottleneck_values
-    return [1,2,3,4,5,6,7,8,9,10]
+    # 这个过程实际上就是将当前图片作为输入计算瓶颈张量的值。这个瓶颈张量的值就是这张图片的新的特征向量。
+    bottleneck_values = sess.run(bottleneck_tensor, {image_data_tensor: image_data})
+    # 经过卷积神经网络处理的结果是一个四维数组，需要将这个结果压缩成一个特征向量（一维数组）
+    bottleneck_values = np.squeeze(bottleneck_values)
+    return bottleneck_values
 
 
 # 这个函数获取一张图片经过Inception-v3模型处理之后的特征向量。
@@ -244,17 +249,18 @@ def get_test_bottlenecks(sess, image_lists, n_classes, jpeg_data_tensor, bottlen
 def main(_):
     # 读取所有图片。
     image_lists = create_image_lists(TEST_PERCENTAGE, VALIDATION_PERCENTAGE)
+    print(image_lists.keys())
+    print(len(image_lists.keys()))
     n_classes = len(image_lists.keys())
-    # # 读取已经训练好的Inception-v3模型。
-    # # 谷歌训练好的模型保存在了GraphDef Protocol Buffer中，里面保存了每一个节点取值的计算方法以及变量的取值。
-    # # TensorFlow模型持久化的问题在第5章中有详细的介绍。
-    # with gfile.FastGFile(os.path.join(MODEL_DIR, MODEL_FILE), 'rb') as f:
-    #     graph_def = tf.GraphDef()
-    #     graph_def.ParseFromString(f.read())
-    # # 加载读取的Inception-v3模型，并返回数据输入所对应的张量以及计算瓶颈层结果所对应的张量。
-    # bottleneck_tensor, jpeg_data_tensor = tf.import_graph_def(graph_def, return_elements=[BOTTLENECK_TENSOR_NAME, JPEG_DATA_TENSOR_NAME])
-    bottleneck_tensor = {}
-    jpeg_data_tensor = {}
+    # 读取已经训练好的Inception-v3模型。
+    # 谷歌训练好的模型保存在了GraphDef Protocol Buffer中，里面保存了每一个节点取值的计算方法以及变量的取值。
+    # TensorFlow模型持久化的问题在第5章中有详细的介绍。
+    with gfile.FastGFile(os.path.join(MODEL_DIR, MODEL_FILE), 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+    # 加载读取的Inception-v3模型，并返回数据输入所对应的张量以及计算瓶颈层结果所对应的张量。
+    bottleneck_tensor, jpeg_data_tensor = tf.import_graph_def(graph_def, return_elements=[BOTTLENECK_TENSOR_NAME,
+                                                                                          JPEG_DATA_TENSOR_NAME])
     # 定义新的神经网络输入，这个输入就是新的图片经过Inception-v3模型前向传播到达瓶颈层时的结点取值。
     # 可以将这个过程类似的理解为一种特征提取。
     bottleneck_input = tf.placeholder(tf.float32, [None, BOTTLENECK_TENSOR_SIZE], name='BottleneckInputPlaceholder')
@@ -276,27 +282,43 @@ def main(_):
         correct_prediction = tf.equal(tf.argmax(final_tensor, 1), tf.argmax(ground_truth_input, 1))
         evaluation_step = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
+    saver = tf.train.Saver()
+
     with tf.Session() as sess:
-        tf.global_variables_initializer().run()
+        if os.path.exists(PARM_FILE + '.index'):
+            # 从文件中恢复变量
+            saver.restore(sess, PARM_FILE)
+            print("Model restored.")
+        else:
+            print("Model inited.")
+            # sess.run(init)
+            tf.global_variables_initializer().run()
+
         # 训练过程
         for i in range(STEPS):
             # 每次获取一个batch的训练数据
             train_bottlenecks, train_ground_truth = get_random_cached_bottlenecks(
                 sess, n_classes, image_lists, BATCH, 'training', jpeg_data_tensor, bottleneck_tensor)
-            sess.run(train_step, feed_dict={bottleneck_input: train_bottlenecks, ground_truth_input: train_ground_truth})
+            sess.run(train_step,
+                     feed_dict={bottleneck_input: train_bottlenecks, ground_truth_input: train_ground_truth})
             # 在验证集上测试正确率。
-            if i%100 == 0 or i+1 == STEPS:
+            if i % 100 == 0 or i + 1 == STEPS:
                 validation_bottlenecks, validation_ground_truth = get_random_cached_bottlenecks(
                     sess, n_classes, image_lists, BATCH, 'validation', jpeg_data_tensor, bottleneck_tensor)
                 validation_accuracy = sess.run(evaluation_step, feed_dict={
-                    bottleneck_input:validation_bottlenecks, ground_truth_input: validation_ground_truth})
+                    bottleneck_input: validation_bottlenecks, ground_truth_input: validation_ground_truth})
                 print('Step %d: Validation accuracy on random sampled %d examples = %.1f%%'
-                      % (i, BATCH, validation_accuracy*100))
+                      % (i, BATCH, validation_accuracy * 100))
+            # 存储变量到文件
+            if i % 1000 == 0:
+                save_path = saver.save(sess, PARM_FILE)
+                print("Model saved in file: ", save_path)
+
         # 在最后的测试数据上测试正确率
         test_bottlenecks, test_ground_truth = get_test_bottlenecks(sess, image_lists, n_classes,
-                                                                       jpeg_data_tensor, bottleneck_tensor)
+                                                                   jpeg_data_tensor, bottleneck_tensor)
         test_accuracy = sess.run(evaluation_step, feed_dict={bottleneck_input: test_bottlenecks,
-                                                                 ground_truth_input: test_ground_truth})
+                                                             ground_truth_input: test_ground_truth})
         print('Final test accuracy = %.1f%%' % (test_accuracy * 100))
 
 
